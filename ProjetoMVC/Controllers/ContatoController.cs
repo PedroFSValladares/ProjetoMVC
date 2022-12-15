e5703367622087f3273794d9fcd75b2fd3fb4562
Pedro Valladares <pedrohvalladares@hotmail.com>
2022-12-14 21:55:53 -0300

    subindo projeto para o GitHub

---
+++ ProjetoMVC/Controllers/ContatoController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Context;
+using ProjetoMVC.Models;
+
+namespace ProjetoMVC.Controllers {
+    public class ContatoController : Controller{
+
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context){ 
+            _context = context; 
+        }
+
+        public IActionResult Index() {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+
+        [HttpGet("Contato/Criar")]
+        public IActionResult Criar() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato) {
+            if(ModelState.IsValid) { 
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(contato);
+        }
+
+        public IActionResult Editar(int id) {
+            var contato = _context.Contatos.Find(id);
+            return contato == null ? NotFound() : View(contato);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Contato contato) {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Numero = contato.Numero;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Detalhes(int id) {
+            var contato = _context.Contatos.Find(id);
+            return View(contato);
+        }
+
+        public IActionResult Deletar(int id) {
+            var contato = _context.Contatos.Find(id);
+            _context.Contatos.Remove(contato);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ ProjetoMVC/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿$(".deletar").click((e) => {
+    e.preventDefault()
+    if (confirm("Tem certeza que deseja deletar este contato\nEssa ação é irreversível.")) {
+        window.location = $(".deletar").attr("href")
+    }
+})
